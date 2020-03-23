d754b57c6d9ceef339789d6df4097ea838c05ef0
reach2arunraj <60217918+reach2arunraj@users.noreply.github.com>
2020-03-24 00:59:19 +0530

    Update same.js

--- JAVA SCRIPT/same.js
+++ JAVA SCRIPT/same.js
@@ -13,3 +13,31 @@ function same(arr1, arr2) {
     return true;
 }
 console.log(same([1, 2, 3, 2], [9, 1, 4, 4]))
+
+
+
+
+function same(arr1, arr2) {
+    if(arr1.length !==arr2.length){
+        return false;
+    }
+
+    let frequencycounter1 = {}
+    let frequencycounter2 = {}
+    for(let val of arr1){
+        frequencycounter1[val] = (frequencycounter1[val] || 0) + 1 
+    }
+    for(let val of arr2){
+        frequencycounter2[val] = (frequencycounter2[val] || 0) + 1
+    }
+    for (let key in frequencycounter1){
+        if(!(key**2 in frequencycounter2)){
+            return false
+        }
+        if (frequencycounter2[key ** 2] !== frequencycounter1[key]){
+            return false
+        }
+    }
+    return true
+}
+console.log(same([1,3,4,6], [36, 1, 16, 9]))
