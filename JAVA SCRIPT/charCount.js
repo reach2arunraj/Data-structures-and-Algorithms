a25b28134a10c72923be9d74df00189f9d82140a
reach2arunraj <60217918+reach2arunraj@users.noreply.github.com>
2020-03-23 21:30:49 +0530

    Update charCount.js

--- JAVA SCRIPT/charCount.js
+++ JAVA SCRIPT/charCount.js
@@ -12,3 +12,22 @@ function charCount(str) {
     return result
 }
 console.log(charCount('ARUN raj !'))
+
+
+
+
+function charCount(str) {
+    var obj = {};
+    for(var char of str){
+        char = char.toLowerCase();
+        if(/[a-z0-9]/.test(char)){
+            if (obj[char] > 0){
+                obj[char]++;
+            }else{
+                obj[char] = 1;
+            }
+        }
+    }
+    return obj
+}
+console.log(charCount("JAVA script check 007"))
