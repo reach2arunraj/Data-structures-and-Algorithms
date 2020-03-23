8fcee9939919f66a19c5d789de02259d96a50345
reach2arunraj <60217918+reach2arunraj@users.noreply.github.com>
2020-03-23 20:54:24 +0530

    Create charCount.js

---
+++ JAVA SCRIPT/charCount.js
@@ -0,0 +1,14 @@
+function charCount(str) {
+    var result = {};
+    for(var i=0; i<str.length; i++){
+        var char = str[i]
+        if (result[char] > 0){
+            result[char]++;
+        }
+        else{
+            result[char] = 1;
+        };
+    }
+    return result
+}
+console.log(charCount('ARUN raj !'))
