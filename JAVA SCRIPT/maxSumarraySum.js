a2c9dcf757b3bc90e4207134c844e87552fa85fd
reach2arunraj <60217918+reach2arunraj@users.noreply.github.com>
2020-03-24 20:16:15 +0530

    Update maxSumarraySum.js

--- JAVA SCRIPT/maxSumarraySum.js
+++ JAVA SCRIPT/maxSumarraySum.js
@@ -15,3 +15,22 @@ function maxSumarraySum(arr, num) {
   return max;
 }
 console.log(maxSumarraySum([1, 2, 3, 4, 5, 6, 7], 3));
+
+
+
+
+function maxSubarraySum(arr, num) {
+  let maxSum = 0;
+  let tempSum = 0;
+  if (arr.length < num) return null;
+  for (let i = 0; i < num; i++) {
+    maxSum += arr[i];
+  }
+  tempSum = maxSum;
+  for (let i = num; i < arr.length; i++) {
+    tempSum = tempSum - arr[i - num] + arr[i];
+    maxSum = Math.max(maxSum, tempSum);
+  }
+  return maxSum;
+}
+console.log(maxSubarraySum([1, 2, 3, 4, 5, 6, 7], 3));
