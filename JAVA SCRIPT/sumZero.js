2c531db1c9fa7966f96a5d235064d3c4d54fdd1d
reach2arunraj <60217918+reach2arunraj@users.noreply.github.com>
2020-03-24 16:28:40 +0530

    Update sumZero.js

--- JAVA SCRIPT/sumZero.js
+++ JAVA SCRIPT/sumZero.js
@@ -9,3 +9,23 @@ function sumZero(arr) {
     return false
 }
 console.log(sumZero([-3, -2, -1, 0, 1, 2, 3]))
+
+
+
+
+function SumZero(arr) {
+    let left = 0;
+    let right = arr.length - 1;
+    while(left < right){
+        let sum = arr[left]+arr[right];
+        if(sum === 0){
+            return [arr[left], arr[right]];
+        }else if(sum > 0){
+            right--;
+        }else{
+            left++;
+        }
+    }
+    return false
+}
+console.log(SumZero([-2, -1, 0, 1, 2]))
