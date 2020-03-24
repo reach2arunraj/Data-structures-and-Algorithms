5a6032a5f0ed1f8bbdf4de99ff616638d0e9f7cd
reach2arunraj <60217918+reach2arunraj@users.noreply.github.com>
2020-03-25 03:40:10 +0530

    Update collectOddValues.js

--- JAVA SCRIPT/Recursion/collectOddValues.js
+++ JAVA SCRIPT/Recursion/collectOddValues.js
@@ -13,3 +13,21 @@ function collectOddValues(arr) {
   return result;
 }
 console.log(collectOddValues([1, 2, 3, 4, 5, 6]));
+
+
+
+
+
+function collectOddValues(arr) {
+    let newarr = [];
+
+    if (arr.length === 0 ){
+        return newarr;
+    }
+    if(arr[0] % 2 !== 0){
+        newarr.push(arr[0]);
+    }
+    newarr = newarr.concat(collectOddValues(arr.slice(1)));
+    return newarr;
+}
+console.log(collectOddValues([1,2,3,4,5,6,7,8,9]))
