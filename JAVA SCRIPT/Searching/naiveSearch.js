51ef20b813bc4a38645679cd83caf49226df2d7c
reach2arunraj <60217918+reach2arunraj@users.noreply.github.com>
2020-03-25 18:54:23 +0530

    Create naiveSearch.js

---
+++ JAVA SCRIPT/Searching/naiveSearch.js
@@ -0,0 +1,11 @@
+function naiveSearch(long, short) {
+    var  count = 0;
+    for(var i = 0; i<long.length; i++){
+        for(var j = 0; j < short.length; j++){
+            if(short[j] !== long[i+j]) break;
+            if (j === short.length -1 ) count++;
+        }
+    }
+    return count;
+}
+console.log(naiveSearch("java java", "ava"))
