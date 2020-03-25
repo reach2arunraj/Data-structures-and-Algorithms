4096d1a975b4a7f7486bd79d7f7940e4f89604b6
reach2arunraj <60217918+reach2arunraj@users.noreply.github.com>
2020-03-25 20:56:52 +0530

    Update sort.js

--- JAVA SCRIPT/Sorting/sort.js
+++ JAVA SCRIPT/Sorting/sort.js
@@ -7,3 +7,15 @@ function numcomp (num1, num2) {
     return num2 - num1;
 }
 [6,2,15,7,20,19].sort(numcomp) // [ 20, 19, 15, 7, 6, 2 ]
+
+
+function compareByLen(str1, str2) {
+    return str1.length - str2.length;
+}
+["apple", "ball", "cow"].sort(compareByLen) // [ "cow", "ball", "apple" ]
+
+
+function compareByLen(str1, str2) {
+    return str2.length - str1.length;
+}
+[ "cow", "ball", "apple" ].sort(compareByLen) // [ "apple", "ball", "cow" ]
