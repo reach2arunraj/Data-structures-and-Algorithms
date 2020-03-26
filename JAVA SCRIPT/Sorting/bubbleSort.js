c67bfd1f78fbf8b7fb474e8e396d290534e4beba
reach2arunraj <60217918+reach2arunraj@users.noreply.github.com>
2020-03-26 21:49:12 +0530

    Update bubbleSort.js

--- JAVA SCRIPT/Sorting/bubbleSort.js
+++ JAVA SCRIPT/Sorting/bubbleSort.js
@@ -12,3 +12,26 @@ function bubbleSort(arr) {
     return arr
 }
 console.log(bubbleSort([12,34,6,78,23,56,90,100]))  
+
+
+
+// Optimizing the code by using noswap
+
+function bubbleSort(arr) {
+    var noswap
+    for (var i = arr.length; i >0; i--){
+        noswap = true;
+        for (var j=0; j<i-1; j++){
+            console.log(arr, arr[j], arr[j+1]);
+            if (arr[j] > arr[j+1]){
+                var temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+                noswap = false;
+            }
+        }
+        if (noswap) break;
+    }
+    return arr
+}
+console.log(bubbleSort([12,34,6,78,23,56,90,100]))  
